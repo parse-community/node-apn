8a926c35f85c62c72a938c77e45ec97e3dbcf853
Corey Baker <coreyearleon@icloud.com>
2025-01-13 13:02:50 -0800

    test

--- test/client.js
+++ test/client.js
@@ -353,7 +353,7 @@ describe('Client', () => {
     expect(infoMessagesContainsPing).to.be.true;
     expect(errorMessages).to.be.empty;
   });
-
+/*
   // https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/handling_notification_responses_from_apns
   it('JSON decodes HTTP 400 responses', async () => {
     let didRequest = false;
@@ -416,7 +416,7 @@ describe('Client', () => {
     ]);
     expect(errorMessages).to.be.empty;
   });
-
+*/
   // node-apn started closing connections in response to a bug report where HTTP 500 responses
   // persisted until a new connection was reopened
   it('Closes connections when HTTP 500 responses are received', async () => {
